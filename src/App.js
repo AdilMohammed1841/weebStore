a2847205a5b1572f3b0ac5e60256fd120e0dc066
Shaik Adhil <adil.shaik@engro.io>
2022-10-10 17:49:52 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login/login";
+import { useState } from 'react';
+import Home from "./components/home/home";
 
 function App() {
+  const [token, setToken] = useState('');
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={token? <Home /> : <Login setToken={setToken} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
