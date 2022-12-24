a37cd0161ef4af1a29abd2d2ac6afbe2b64385be
Shaik Adhil <adil.shaik@engro.io>
2022-12-24 12:06:16 +0530

    in-progress

--- src/components/details/contentDetails.js
+++ src/components/details/contentDetails.js
@@ -1,37 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { CompData } from '../../utils/includes';
-import { useParams } from 'react-router-dom';
-import ReactPlayer from 'react-player/youtube'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { CompData } from "../../utils/includes";
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { ReactVideoPlayer } from "video-player-for-react";
+import "video-player-for-react/dist/index.css";
 
 // Only loads the YouTube player
 
 const ContentDetails = () => {
-    const [details, setDetails] = useState([]);
-    let { slug } = useParams();
+  const [details, setDetails] = useState([]);
+  let { slug } = useParams();
 
-    useEffect(() => {
-        axios.get(`${CompData.GETURL}?filter[slug]=${slug}`).then((response) => {
-          setDetails(response.data);
-        })
-    },[])
-    return (
-        <div>
-            {
-                details && details?.data && details?.data.map((item,index) => {
-                    return (
-                        <>
-                            {item?.attributes?.youtubeVideoId ?
-                                <ReactPlayer url={`https://www.youtube.com/watch?v=${item?.attributes?.youtubeVideoId}`} />
-                                :
-                                <p>Video Not Found  </p>
-                            }
-                        </>
-                    )
-                })
-            }
-        </div>
-    )
-}
+  useEffect(() => {
+    axios.get(`${CompData.GETURL}?filter[slug]=${slug}`).then((response) => {
+      setDetails(response.data);
+    });
+  }, []);
+  return (
+    <div>
+      {details &&
+        details?.data &&
+        details?.data.map((item, index) => {
+          return (
+            <>
+              {item?.attributes?.youtubeVideoId ? (
+                // <ReactPlayer url={`https://www.youtube.com/watch?v=${item?.attributes?.youtubeVideoId}`} />
+                // <ReactPlayer url={"https://gogoanime.lu//one-piece-episode-1026"} />
+                <ReactVideoPlayer
+                        width='928px'
+                        url="https://gogohd.net/streaming.php?id=MzUxOA==&title=One+Piece+Episode+1"
+                        // type='video/mp4'
+                        // poster='http://comondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'
+                        // captions={[
+                        //   {
+                        //     kind: 'captions',
+                        //     label: 'English',
+                        //     srcLang: 'en',
+                        //     src: 'caption_url'
+                        //   }
+                        // ]}
+                />
+              ) : (
+                <p>Video Not Found </p>
+              )}
+            </>
+          );
+        })}
+    </div>
+  );
+};
 
-export default ContentDetails
+export default ContentDetails;
